541dd9fe783889494dda9f575c58d4d169030eab
daisuke <daisuke5220@gmail.com>
2019-08-20 09:51:26 -0500

    Team JS

--- js/app.js
+++ js/app.js
@@ -40,5 +40,56 @@ $(document).ready(function () {
 
     });
 
-    // array like wrapper
+    // team members
+    (function (){
+        let data = [
+         {
+          id: 0,
+          name: 'john doe',
+          job: 'developer',
+          text: `A biography, or simply bio, is a detailed description of a person's life.It involves more than just the basic facts like education, work, relationships, and death; it portrays a person's experience of these life events. Unlike a profile or curriculum vitae (résumé), a biography presents a subject's life story, highlighting various aspects of his or her life, including intimate details of experience, and may include an analysis of the subject's personality.`,
+          favorites: ['eat', 'drink', 'sleep'],
+          img: 'img/team-1.jpg'
+         },
+         {
+          id: 1,
+          name: 'tom orange',
+          job: 'designer',
+          text: `A biography, or simply bio, is a detailed description of a person's life.It involves more than just the basic facts like education, work, relationships, and death; it portrays a person's experience of these life events. Unlike a profile or curriculum vitae (résumé), a biography presents a subject's life story, highlighting various aspects of his or her life, including intimate details of experience, and may include an analysis of the subject's personality.`,
+          favorites: ['paint', 'draw', 'run'],
+          img: 'img/team-2.jpg'
+         },
+         {
+          id: 2,
+          name: 'albert cupid',
+          job: 'accountant',
+          text: `A biography, or simply bio, is a detailed description of a person's life.It involves more than just the basic facts like education, work, relationships, and death; it portrays a person's experience of these life events. Unlike a profile or curriculum vitae (résumé), a biography presents a subject's life story, highlighting various aspects of his or her life, including intimate details of experience, and may include an analysis of the subject's personality.`,
+          favorites: ['math', 'physics', 'more math'],
+          img: 'img/team-3.jpg'
+         },
+         {
+          id: 4,
+          name: 'dog hugo',
+          job: 'intern',
+          text: `A biography, or simply bio, is a detailed description of a person's life.It involves more than just the basic facts like education, work, relationships, and death; it portrays a person's experience of these life events. Unlike a profile or curriculum vitae (résumé), a biography presents a subject's life story, highlighting various aspects of his or her life, including intimate details of experience, and may include an analysis of the subject's personality.`,
+          favorites: ['bark', 'run', 'bite'],
+          img: 'img/team-4.jpg'
+         }]
+
+        $('.team-img__item').click(function(){
+            // adding/removing active class
+            $('.team-img__item').not(this).removeClass('active');
+            $(this).addClass('active');
+
+            // change data
+            let id = $(this).attr('data-id');
+            $('.team-info__member-name').text(data[id].name);
+            $('.team-info__member-job').text(data[id].job);
+            $('.team-info__member-text').text(data[id].text);
+            $('.team-info__member-single-favorite-text').each(function(index){
+                $(this).text(data[id].favorites[index]);
+            });
+            $('.team-info__img').attr('src', data[id].img);
+        });
+    })();
 })
