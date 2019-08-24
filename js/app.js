0810322a21a584c6cdfa5d35c44304333256c489
daisuke <daisuke5220@gmail.com>
2019-08-24 11:54:12 -0500

    Quotes JS

--- js/app.js
+++ js/app.js
@@ -92,4 +92,24 @@ $(document).ready(function () {
             $('.team-info__img').attr('src', data[id].img);
         });
     })();
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        smartSpeed: 2000,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+    $(".owl-prev").html('<div><i class="fa fa-chevron-left"></i></div>');
+    $(".owl-next").html('<div><i class="fa fa-chevron-right"></i></div>');
 })
